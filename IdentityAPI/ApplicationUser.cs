788a5c57494c6c0d33ffb9c83a21330fce06cf72
skillednoob <amol05jd@gmail.com>
2025-04-26 17:02:21 +0530

    feat: Setup initial database and identity
    Adds ApplicationDbContext, ApplicationUser, configures database connection, registers services, installs necessary packages, and applies initial migration.

---
+++ IdentityAPI/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityAPI
+{
+	public class ApplicationUser : IdentityUser
+	{
+	}
+}
--- IdentityAPI/Program.cs
+++ IdentityAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace IdentityAPI
 {
 	public class Program
@@ -8,6 +11,15 @@ namespace IdentityAPI
 			var builder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
+
+			// Add services to the container.(26-Apr-25)
+			builder.Services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+			// Add ASP.NET Core Identity(26-Apr-25)
+			builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddDefaultTokenProviders();
 
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
